a55d551ffaf0da2e8f11a6ce9ca24c2d593d9d43
Thomas Roberts <thomas.roberts@automattic.com>
2024-07-09 17:53:28 +0100

    Update ExtensionCartUpdateArgs to allow overwriting dirty data

--- plugins/woocommerce-blocks/assets/js/types/type-defs/cart.ts
+++ plugins/woocommerce-blocks/assets/js/types/type-defs/cart.ts
@@ -217,6 +217,7 @@ export interface CartMeta {
 export interface ExtensionCartUpdateArgs {
 	data: Record< string, unknown >;
 	namespace: string;
+	overwriteDirtyCustomerData?: undefined | boolean;
 }
 
 export interface BillingAddressShippingAddress {
