502cf945b279809a649ee0068606204e8c39e26a
Maikel Perez <mdperez86@gmail.com>
2024-08-22 14:37:15 -0400

    Using woocommerceOnSale as boolead | undefined to allow a 3rd state when filter by on sale

--- plugins/woocommerce-blocks/assets/js/blocks/product-collection/types.ts
+++ plugins/woocommerce-blocks/assets/js/blocks/product-collection/types.ts
@@ -75,7 +75,12 @@ export interface ProductCollectionQuery {
 	 */
 	featured: boolean;
 	timeFrame: TimeFrame | undefined;
-	woocommerceOnSale: boolean;
+	/**
+	 * If true: it shows only on sale products.
+	 * If false: it hides/excludes on sale products.
+	 * If undefined: the filter is inactive.
+	 */
+	woocommerceOnSale: boolean | undefined;
 	/**
 	 * Filter products by their stock status.
 	 *
